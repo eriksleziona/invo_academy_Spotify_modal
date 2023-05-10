831e791271b214652d7b3cd8c1b466c9f099ecdc
Erik Sleziona <erik.sleziona@gmail.com>
2023-05-10 06:46:11 +0200

    Initial commit

---
+++ src/pages/home-page/home-page.js
@@ -0,0 +1,9 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import "./home-page.scss";
+import { UserFeedbackModal } from "./components/user-feedback-modal/user-feedback-modal";
+
+setTimeout(() => {
+  UserFeedbackModal();
+}, 2000);
