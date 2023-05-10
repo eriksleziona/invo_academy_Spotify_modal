ecae93409494792ed3a0d4ba1238b5417c85075e
Erik Sleziona <erik.sleziona@gmail.com>
2023-05-10 12:27:56 +0200

    Adding media Queries etc

---
+++ src/pages/home-page/components/user-feedback-component/user-feedback-component.js
@@ -0,0 +1,64 @@
+import Swal from "sweetalert2";
+const UserFeedbackComponent = () => {
+  let currentRating = null;
+  const userFeedbackComponent = document.querySelector(
+    ".user-feedback-component"
+  );
+  const allRatesTiles = userFeedbackComponent.querySelectorAll(
+    ".user-feedback-component__single-rate-tile"
+  );
+
+  const confirmButton = document.querySelector(
+    ".user-feedback-component__confirm-button"
+  );
+
+  addListenersToAllRatesTiles();
+
+  confirmButton.addEventListener("click", () => {
+    const hide = document.querySelector(".btn-primary__text");
+    const unhide = document.querySelector(".btn-primary__loader");
+    hide.toggleAttribute("data-loading");
+    unhide.toggleAttribute("data-loading");
+    setTimeout(() => {
+      Swal.clickConfirm();
+    }, 2000);
+  });
+
+  function addListenersToAllRatesTiles() {
+    allRatesTiles.forEach((singleRateTile) => {
+      singleRateTile.addEventListener("click", () => {
+        singleRateTile.toggleAttribute("data-selected");
+        if (singleRateTile.hasAttribute("data-selected")) {
+          currentRating = singleRateTile.getAttribute("data-rate");
+        } else {
+          currentRating = null;
+        }
+
+        console.log(`current rating: ${currentRating}`);
+        afterRatingChanged(singleRateTile);
+      });
+    });
+  }
+
+  function disselectOtherRatings(clickedTile) {
+    allRatesTiles.forEach((singleRateTile) => {
+      if (singleRateTile != clickedTile) {
+        singleRateTile.removeAttribute("data-selected");
+      }
+    });
+  }
+
+  function setProperButtonState() {
+    if (currentRating) {
+      confirmButton.removeAttribute("disabled");
+    } else {
+      confirmButton.setAttribute("disabled", true);
+    }
+  }
+  function afterRatingChanged(singleRateTile) {
+    disselectOtherRatings(singleRateTile);
+    setProperButtonState(currentRating);
+  }
+};
+
+export default UserFeedbackComponent;
--- src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
+++ src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
@@ -1,53 +1,49 @@
 import Swal from "sweetalert2";
-
+import UserFeedbackComponent from "../user-feedback-component/user-feedback-component";
 export const UserFeedbackModal = () => {
-  let currentRating = null;
-  const userFeedbackComponent = document.querySelector(
-    ".user-feedback-component"
-  );
-  const allRatesTiles = userFeedbackComponent.querySelectorAll(
-    ".user-feedback-component__single-rate-tile"
+  let userFeedbackModalClosedTimestamp = localStorage.getItem(
+    "userFeedbackModalClosedTimestamp"
   );
 
-  const confirmButton = document.querySelector(
-    ".user-feedback-component__confirm-button"
-  );
+  // Check if the user has seen the modal before and it has been less than 3 days since then
+  if (userFeedbackModalClosedTimestamp) {
+    // Define the number of milliseconds in 3 days - it is needed, because the getTime() method
+    // of the Date object returns the number of milliseconds since January 1, 1970, 00:00:00 UTC.
+    // This allows us to compare timestamps accurately
+    const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000;
+    const currentDate = new Date();
 
-  addListenersToAllRatesTiles();
+    // Subtract the number of milliseconds in 3 days from the current date
+    // to get the date 3 days ago
+    const threeDaysAgo = currentDate.getTime() - threeDaysInMilliseconds;
 
-  function addListenersToAllRatesTiles() {
-    allRatesTiles.forEach((singleRateTile) => {
-      singleRateTile.addEventListener("click", () => {
-        singleRateTile.toggleAttribute("data-selected");
-        if (singleRateTile.hasAttribute("data-selected")) {
-          currentRating = singleRateTile.getAttribute("data-rate");
-        } else {
-          currentRating = null;
-        }
-
-        console.log(`current rating: ${currentRating}`);
-        afterRatingChanged(singleRateTile);
-      });
-    });
-  }
-
-  function disselectOtherRatings(clickedTile) {
-    allRatesTiles.forEach((singleRateTile) => {
-      if (singleRateTile != clickedTile) {
-        singleRateTile.removeAttribute("data-selected");
-      }
-    });
-  }
-
-  function setProperButtonState() {
-    if (currentRating) {
-      confirmButton.removeAttribute("disabled");
-    } else {
-      confirmButton.setAttribute("disabled", true);
+    if (userFeedbackModalClosedTimestamp >= threeDaysAgo) {
+      // If it has been less than 3 days, return to exit the function
+      // without displaying the modal
+      return;
     }
   }
-  function afterRatingChanged(singleRateTile) {
-    disselectOtherRatings(singleRateTile);
-    setProperButtonState(currentRating);
-  }
+
+  Swal.fire({
+    template: "#user-feedback-modal",
+    showConfirmButton: false,
+    width: "492px",
+    padding: 0,
+    customClass: {
+      container: "user-feedback-modal",
+    },
+    background: "var(--background-color)",
+    color: "var(--on-background-color)",
+    showCloseButton: true,
+    allowEnterKey: false,
+    closeButtonHtml: '<img src="/assets/icons/close-icon.svg" alt="X" />',
+    didClose: () => {
+      localStorage.setItem("userFeedbackModalClosedTimestamp", Date.now());
+    },
+    isConfirm: () => {
+      localStorage.setItem("userFeedbackModalClosedTimestamp", Date.now());
+    },
+  });
+
+  UserFeedbackComponent();
 };
