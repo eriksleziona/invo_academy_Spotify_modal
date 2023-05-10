3cfd25a2f1a26a495a3b1786adacb410cc2b1b8c
Erik Sleziona <erik.sleziona@gmail.com>
2023-05-10 09:09:06 +0200

    Adding basic Js control for reading data and changing styling

--- src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
+++ src/pages/home-page/components/user-feedback-modal/user-feedback-modal.js
@@ -1,10 +1,53 @@
 import Swal from "sweetalert2";
 
 export const UserFeedbackModal = () => {
-  Swal.fire({
-    title: "Error!",
-    text: "Do you want to continue",
-    icon: "error",
-    confirmButtonText: "Cool",
-  });
+  let currentRating = null;
+  const userFeedbackComponent = document.querySelector(
+    ".user-feedback-component"
+  );
+  const allRatesTiles = userFeedbackComponent.querySelectorAll(
+    ".user-feedback-component__single-rate-tile"
+  );
+
+  const confirmButton = document.querySelector(
+    ".user-feedback-component__confirm-button"
+  );
+
+  addListenersToAllRatesTiles();
+
+  function addListenersToAllRatesTiles() {
+    allRatesTiles.forEach((singleRateTile) => {
+      singleRateTile.addEventListener("click", () => {
+        singleRateTile.toggleAttribute("data-selected");
+        if (singleRateTile.hasAttribute("data-selected")) {
+          currentRating = singleRateTile.getAttribute("data-rate");
+        } else {
+          currentRating = null;
+        }
+
+        console.log(`current rating: ${currentRating}`);
+        afterRatingChanged(singleRateTile);
+      });
+    });
+  }
+
+  function disselectOtherRatings(clickedTile) {
+    allRatesTiles.forEach((singleRateTile) => {
+      if (singleRateTile != clickedTile) {
+        singleRateTile.removeAttribute("data-selected");
+      }
+    });
+  }
+
+  function setProperButtonState() {
+    if (currentRating) {
+      confirmButton.removeAttribute("disabled");
+    } else {
+      confirmButton.setAttribute("disabled", true);
+    }
+  }
+  function afterRatingChanged(singleRateTile) {
+    disselectOtherRatings(singleRateTile);
+    setProperButtonState(currentRating);
+  }
 };
